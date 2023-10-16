a6a5f0f422bbc740dbe1ca49ee75441c6dd5ce47
Ajand Mardalizad <aseedgeek@gmail.com>
2023-10-16 22:26:02 +0400

    updated script

--- deploy.sh
+++ deploy.sh
@@ -23,23 +23,31 @@ mkdir db
 mkdir files
 mkdir files/wallets
 
-cd lib/rai-analytics-server
+cd ..
+git clone https://github.com/ajand/keeper-manager-analytics-server.git rai-analytics-server
+cd rai-analytics-server
 docker build -t rai-analytics-server .
-echo "rai-analytics-server server image built"
 cd ..
-cd rai-keeper
-docker build -t rai-keeper .
-echo "rai-keeper image built"
-cd ..
+
+git clone https://github.com/money-god/auction-keeper.git tai-keeper
 cd tai-keeper
+git submodule update --force --recursive --init --remote
 docker build -t tai-keeper .
-echo "tai-keeper image built"
 cd ..
+
+git clone https://github.com/reflexer-labs/auction-keeper.git rai-keeper
+cd rai-keeper
+git submodule update --force --recursive --init --remote
+docker build -t rai-keeper .
+cd ..
+
+git cloen https://github.com/Ajand/GEB-Keeper-Manager-client.git client
 cd client
 docker build -t manager-client .
 echo "manager-client image built"
+
 cd ..
-cd ..
+cd keeper-manager
 docker build -t manager-server .
 echo "manager-service image built"
 
